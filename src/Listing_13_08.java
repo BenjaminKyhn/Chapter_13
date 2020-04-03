1147c6138d45c7589da0704975114571693891c3
Benjamin Kyhn <benkyhn@hotmail.com>
2020-04-03 17:26:19 +0200

    Added listing 13.8

--- src/Listing_13_08.java
+++ src/Listing_13_08.java
@@ -6,13 +6,13 @@ public class Listing_13_08 {
         String[] cities = {"Savannah", "Boston", "Atlanta", "Tampa"};
         Arrays.sort(cities);
         for (String city : cities)
-            System.out.println(city + " ");
+            System.out.print(city + " ");
             System.out.println();
 
         BigInteger[] hugeNumbers = {new BigInteger("2323231092923992"), new BigInteger("432232323239292"),
                 new BigInteger("54623239292")};
         Arrays.sort(hugeNumbers);
         for (BigInteger number: hugeNumbers)
-            System.out.println(number + " ");
+            System.out.print(number + " ");
     }
 }
