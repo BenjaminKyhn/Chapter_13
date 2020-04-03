61b2939c297f36eeb0e02e904c0ba239c84f5528
Benjamin Kyhn <benkyhn@hotmail.com>
2020-04-03 19:03:55 +0200

    Added listing 13.13

--- src/House.java
+++ src/House.java
@@ -1,36 +1,36 @@
 import java.util.Date;
 
-public class House implements Cloneable, Comparable<House>{
+public class House implements Cloneable, Comparable<House> {
     private int id;
     private double area;
     private Date whenBuilt;
 
-    public House(int id, double area){
+    public House(int id, double area) {
         this.id = id;
         this.area = area;
         whenBuilt = new Date();
     }
 
-    public int getId(){
+    public int getId() {
         return id;
     }
 
-    public double getArea(){
+    public double getArea() {
         return area;
     }
 
-    public Date getWhenBuilt(){
+    public Date getWhenBuilt() {
         return whenBuilt;
     }
 
-    @Override /** Override the protected clone method defined in the Object class, and strengthen its accessibility */
-    public Object clone(){
-        try{
-            return super.clone();
-        }
-        catch (CloneNotSupportedException ex){
-            return null;
-        }
+    @Override
+    /** Override the protected clone method defined in the Object class, and strengthen its accessibility */
+    public Object clone() throws CloneNotSupportedException {
+        // Perform a shallow copy
+        House houseClone = (House) super.clone();
+        // Deep copy on whenBuilt
+        houseClone.whenBuilt = (java.util.Date) (whenBuilt.clone());
+        return houseClone;
     }
 
     @Override // Implement the compareTo method defined in Comparable
---
+++ src/Listing_13_12.java
@@ -0,0 +1,11 @@
+public class Listing_13_12 {
+    /** Main method */
+    public static void main(String[] args) {
+        // Create and initialize two rational numbers r1 and r2
+        Rational r1 = new Rational(4, 2);
+        Rational r2 = new Rational(2, 3);
+
+        // Display results
+        System.out.println(r1 + " + " + r2 + " = " + r1.add(r2));
+    }
+}
